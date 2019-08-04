dcb645ca70b9564b2d042ffeaef8bf56d3d9e601
aravindanath <aravindanath86@gmail.com>
2019-08-04 11:11:33 +0530

    updated basepage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,12 +13,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.Reporter;
 
 import com.google.common.io.Files;
 
 public class BasePage {
 
-	private WebDriver driver;
+	private static WebDriver driver;
 
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
@@ -29,7 +31,13 @@ public class BasePage {
 		action.moveToElement(element).build().perform();
 	}
 
-	public void takeSnapShot() throws Exception {
+ 
+	public static void assertPageTitle(String actual, String expected) {
+		Assert.assertEquals(actual, expected, " Mismatch in Actual vs Expected!");
+	}
+
+	
+	public static void takeSnapShot() throws Exception {
 		// Convert web driver object to TakeScreenshot
 		TakesScreenshot scrShot = ((TakesScreenshot) driver);
 		// Call getScreenshotAs method to create image file
@@ -37,7 +45,7 @@ public class BasePage {
 		Date d = new Date();
 		System.out.println(d);
 		String screenshotFile = d.toString().replace(":", "_").replace(" ", "_") + ".png";
-		String path = System.getProperty("user.dir") + "/Output/";
+		String path = System.getProperty("user.dir") + "/test-output/";
 		String filePath = path + screenshotFile;
 		// Move image file to new destination
 		File DestFile = new File(filePath);
@@ -46,7 +54,7 @@ public class BasePage {
 
 	}
 
-	public String captureScreen() throws IOException {
+	public static String captureScreen() throws IOException {
 		TakesScreenshot screen = (TakesScreenshot) driver;
 		File src = screen.getScreenshotAs(OutputType.FILE);
 		String dest = System.getProperty("user.dir") + "/Output/screenShot.png";
@@ -55,8 +63,89 @@ public class BasePage {
 		return dest;
 	}
 
-	public static void assertPageTitle(String actual, String expected) {
+	public static void assertPageTitle(String expected) {
+		String actual = driver.getTitle();
+		Reporter.log("Actual title: " + actual);
 		Assert.assertEquals(actual, expected, " Mismatch in Actual vs Expected!");
+	}
+
+	/**
+	 * @author aravindanathdm THis method is used to click on webelement
+	 * @param element
+	 */
+	public static void click(WebElement element) {
+		Actions act = new Actions(driver);
+		act.click(element).build().perform();
+	}
+
+	public static void jsClick(WebElement element) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].click();", element);
+
+	}
+
+	public static void drawBorder(WebElement element, WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].style.border='3px solid red'", element);
+	}
+
+	public static void generateAlert(WebDriver driver, String message) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("alert('" + message + "')");
+
+	}
+
+	public static void clickElementByJS(WebElement element, WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].click();", element);
+
+	}
+
+	public static void refreshBrowserByJS(WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("history.go(0)");
+	}
+
+	public static String getTitleByJS(WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		String title = js.executeScript("return document.title;").toString();
+		return title;
+	}
+
+	public static String getPageInnerText(WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		String pageText = js.executeScript("return document.documentElement.innerText;").toString();
+		return pageText;
+	}
+
+	public static void scrollPageDown(WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+	}
+
+	public static void scrollIntoView(WebElement element, WebDriver driver) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+
+	public static long generateMobile() {
+		long mob = (long) (Math.random() * 9999999999l);
+		return mob;
+	}
+
+	public String getTodayDate() {
+		Date d = new Date();
+		return d.toString();
+	}
+
+	public void rightClick(WebElement element) {
+		Actions act = new Actions(driver);
+		act.contextClick(element).build().perform();
+	}
+
+	public void dragNDrop(WebElement src, WebElement tgt) {
+		Actions act = new Actions(driver);
+		act.dragAndDrop(src, tgt).build().perform();
 	}
 
 }
